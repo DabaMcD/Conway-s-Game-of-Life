ce518366610f166f622a6fc10036fcff350cbd41
gooberbobber <dabamcdoodle@gmail.com>
2018-12-17 15:08:36 -0300

    wip

--- app/src/main/java/com/conwaysgame/oflife/conwaysgameoflife/MainActivity.java
+++ app/src/main/java/com/conwaysgame/oflife/conwaysgameoflife/MainActivity.java
@@ -2,12 +2,18 @@ package com.conwaysgame.oflife.conwaysgameoflife;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.Touch;
 
 public class MainActivity extends AppCompatActivity {
+    GameView gameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        gameView = findViewById(R.id.gameView);
+//        gameView.constructor();
+//        Touch.setTouchListener(gameView, gameView);
     }
 }
